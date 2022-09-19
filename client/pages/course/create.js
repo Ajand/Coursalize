6c01d0e0e58a35acaab8d24c201d937f858e3910
Ajand Mardalizad <aseedgeek@gmail.com>
2022-09-19 16:48:54 +0430

    course create page layout added

--- client/pages/course/create.js
+++ client/pages/course/create.js
@@ -2,12 +2,28 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import Header from "../../components/Header";
+import { Container, Grid, Paper, TextField, Button } from "@mui/material";
 
 const CreateCourse = () => {
   return (
     <div>
       <Header />
-      This is gonna be the create course page
+      <Container>
+        <Grid container>
+          <Grid item md={3}></Grid>
+          <Grid item md={6}>
+            <Paper
+              css={css`
+                margin-top: 4em;
+                padding: 2em;
+              `}
+            >
+              This is gonna be the create course form
+            </Paper>
+          </Grid>
+          <Grid item md={3}></Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
