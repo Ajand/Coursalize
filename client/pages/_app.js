b707b434f7420fb9b937fdf3aefce54c2a14387f
Ajand Mardalizad <aseedgeek@gmail.com>
2022-09-19 15:23:15 +0430

    mui added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+const darkTheme = createTheme({
+  typography: {},
+  palette: {},
+  components: {
+    MuiPaper: {},
+    MuiButton: {},
+    MuiInputBase: {},
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />{" "}
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
